633aae93b73de97b307b0deb6719ea9dfd120626
GuanlinLu <xuanxs@yeah.net>
2023-07-17 16:18:02 -0400

    practice_update

--- code/01-starting-setup/src/App.js
+++ code/01-starting-setup/src/App.js
@@ -1,7 +1,14 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
+  //imperative (traditional js)
+  // const para = document.createElement('p');
+  // para.textContent= 'test abc';
+  // document.getElementById('root').append(para);
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Let's get started!!!</h2>
+      <ExpenseItem></ExpenseItem>
     </div>
   );
 }
